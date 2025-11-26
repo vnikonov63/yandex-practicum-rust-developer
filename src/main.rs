d1af9862c3469f59567a8933bc921bfcacf32639
Vasilii Nikonov <nikonvasilii@gmail.com>
2025-11-25 19:08:05 -0800

    Finished 0-2-5 Yandex Practicum Rust Course
    We have covered patter-mathcing with `match` and such interesting
    constructions as: `if let`, `while let`, `let else`. I am debating
    whether to start writing a commit message for every lesson completed, to
    boost the activity overview graph on Github or not. I am also practicing
    using LazyGit with nvim - and so I want to write as many messages as
    possible.
    
    I really find the experience with nvim cool. It feels ergonomic. I also
    like the idea, that I will feel OK, even if I have to work on a remote
    computer, and I can only ssh into it.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 mod config;
 mod utils;
 
-use config::DEFAULT_COURSE_NAME;
-use time::OffsetDateTime;
-use utils::helpers::greet;
+//use config::DEFAULT_COURSE_NAME;
+//use time::OffsetDateTime;
+//use utils::helpers::greet;
+use utils::helpers::show_progress;
 
 fn main() {
-    greet();
+    //greet();
+    //println!("Today: {}", OffsetDateTime::now_utc().date());
+    //println!("I am completing the course: {}", DEFAULT_COURSE_NAME);
 
-    println!("Today: {}", OffsetDateTime::now_utc().date());
-    println!("I am completing the course: {}", DEFAULT_COURSE_NAME);
+    println!("My progress in the current module:");
+
+    show_progress(5, 15);
 }
--- src/utils/helpers.rs
+++ src/utils/helpers.rs
@@ -1,3 +1,22 @@
 pub fn greet() {
     println!("Hello world! from a spearate file!");
 }
+
+pub fn show_progress(current: u32, total: u32) {
+    const WIDTH: u32 = 20;
+
+    let percentage = current * 100 / total;
+    let filled = percentage * WIDTH / 100;
+
+    let mut bar = String::new();
+
+    for i in 0..WIDTH {
+        if i < filled {
+            bar.push('█');
+        } else {
+            bar.push(' ');
+        }
+    }
+
+    println!("[{}] {}%", bar, percentage);
+}
