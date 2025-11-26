a7f58362db7fea41aaec263284bf5103a05fd6c6
Vasilii Nikonov <nikonvasilii@gmail.com>
2025-11-25 23:47:57 -0800

    Finished 0-3-1
    Here we managed to go though closures. Like I understand how to write
    basic ones. But we have covered three types of closured, based on traits
    Fn, FnMut, FnOnce. I do not understand that. Like isn't trait supposed
    to be something that not only closures, but other things as well can
    "interact with". I think I would need to return to this chapter again.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,19 @@ use config::CourseConfig;
 use time::OffsetDateTime;
 use utils::helpers::{greet, show_progress};
 
+// TODO: Think of a better place to store the exercises, so I can have many main functions, e.t.c
+// Exercise from 0.3.1
+fn make_adder(n: i32) -> impl Fn(i32) -> i32 {
+    move |x: i32| x + n
+}
+
 fn main() {
+    let add5 = make_adder(5);
+    let add10 = make_adder(10);
+
+    println!("{}", add5(3));
+    println!("{}", add10(3));
+
     let course = CourseConfig::default();
 
     greet();
