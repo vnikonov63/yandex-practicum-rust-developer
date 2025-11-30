4ee5cc50123b0a70bcd473575c120db331fb4fe8
Vasilii Nikonov <nikonvasilii@gmail.com>
2025-11-29 21:04:53 -0800

    Finished 0-3-2
    In this chapter we have covered the `Option<T>` and `Result<T, E` and
    the syntactic sugar operator `?`. I understand the concept of Rust - we
    want to `panic!` if something failed (actually is not reasonable
    anymore), but are required to deal with all the other cases.
    
    I still do not understand the concept of the trait (although it has
    something to do with the shared behaviour). Like it was mentioned in the
    reading, that `std::error:Error` is a trait, that guarantees the shared
    behaviour of the errors from different rust projects. Like if we define
    our custom error as:
    
    ```
    #[derive(Debug, PartialEq)]
    enum MathError
    ```
    
    why does this trait matter, or osmething is happening behind the scenes?

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,46 @@ use time::OffsetDateTime;
 use utils::helpers::{greet, show_progress};
 
 // TODO: Think of a better place to store the exercises, so I can have many main functions, e.t.c
+
 // Exercise from 0.3.1
 fn make_adder(n: i32) -> impl Fn(i32) -> i32 {
     move |x: i32| x + n
 }
 
+// Exercise from 0.3.2.1
+fn find_first_even(numbers: &[i32]) -> Option<i32> {
+    numbers.iter().find(|&&n| n % 2 == 0).copied()
+}
+
+// Exercise from 0.3.2.2
+#[derive(Debug, PartialEq)]
+enum MathError {
+    DivisionByZero,
+}
+
+fn safe_divide(a: f64, b: f64) -> Result<f64, MathError> {
+    if b == 0.0 {
+        Err(MathError::DivisionByZero)
+    } else {
+        Ok(a / b)
+    }
+}
+
 fn main() {
+    // Exercise from 0.3.1
     let add5 = make_adder(5);
     let add10 = make_adder(10);
 
     println!("{}", add5(3));
     println!("{}", add10(3));
+
+    // Exercise from 0.3.2.1
+    assert_eq!(find_first_even(&[1, 3, 4, 7, 8]), Some(4));
+    assert_eq!(find_first_even(&[1, 3, 5, 7]), None);
+
+    // Exercise from 0.3.2.2
+    assert_eq!(safe_divide(10.0, 2.0), Ok(5.0));
+    assert_eq!(safe_divide(10.0, 0.0), Err(MathError::DivisionByZero));
 
     let course = CourseConfig::default();
 
